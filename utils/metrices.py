02b78fc43bda39dc31ef3f5d1f06c16028a1a510
RamzeyChibana <ramzichibana31@gmail.com>
2024-10-04 21:11:47 +0100

    Fix bugs

--- utils/metrices.py
+++ utils/metrices.py
@@ -15,15 +15,17 @@ def Percision_at_k(batch_rates,k):
 def dgc_at_k(batch_rates,k):
     batch_rates = batch_rates[:,:k]
     log = np.log2(np.arange(1+1,k+2))
+    
     return np.sum(batch_rates/log,axis=1)
 
 
 def Ndgc_at_k(batch_rates,k):
     batch_rates = np.asfarray(batch_rates[:,:k])
     dgc = dgc_at_k(batch_rates,k)
-    ideal_rates = np.argsort(batch_rates,axis=1)[:,::-1]
+    ideal_rates = np.sort(batch_rates,axis=1)[:,::-1]
     idgc = dgc_at_k(ideal_rates,k)
-    return np.sum(dgc/idgc)
+    ndgc = np.divide(dgc,idgc,out=np.zeros_like(dgc),where=idgc!=0)
+    return np.sum(ndgc)
 
 
 
